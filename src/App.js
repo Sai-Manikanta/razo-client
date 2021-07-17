f1eab0f52db04c6660d7c229aa7f3bca0cb34ba9
mani333007 <mani333007@gmail.com>
2021-07-17 13:03:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function loadScript(src) {
+	return new Promise((resolve) => {
+		const script = document.createElement('script')
+		script.src = src
+		script.onload = () => {
+			resolve(true)
+		}
+		script.onerror = () => {
+			resolve(false)
+		}
+		document.body.appendChild(script)
+	})
 }
 
-export default App;
+const __DEV__ = document.domain === 'localhost'
+
+function App() {
+	const [name, setName] = useState('Mani')
+
+	async function displayRazorpay() {
+		const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+
+		if (!res) {
+			alert('Razorpay SDK failed to load. Are you online?')
+			return
+		}
+
+		const data = await fetch('http://localhost:9000/razorpay', { method: 'POST' }).then((t) =>
+			t.json()
+		)
+
+		const options = {
+			key: __DEV__ ? 'rzp_test_9Gn06P6pqobHOH' : 'PRODUCTION_KEY',
+			currency: data.currency,
+			amount: data.amount.toString(),
+			order_id: data.id,
+			name: 'Stock calls',
+			description: 'Get awesome stock calls',
+			image: 'https://raw.githubusercontent.com/mehulmpt/razorpay-payments-tutorial/8141a0d6435b9fa648725a40fcee92fa83820546/backend/logo.svg',
+			handler: function (response) {
+				// alert(response.razorpay_payment_id)
+				// alert(response.razorpay_order_id)
+				// alert(response.razorpay_signature)
+				console.log('---')
+				console.log(response);
+			},
+			prefill: {
+				name,
+				// email: 'sdfdsjfh2@ndsfdf.com',
+				// phone_number: '9899999999'
+			}
+		}
+		const paymentObject = new window.Razorpay(options)
+		paymentObject.open()
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					onClick={displayRazorpay}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Donate 
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
